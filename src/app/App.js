da204c4fcc1a70121d874290e79b7a89f5b0ce3f
Jeffrey Paulino <jp7kode@gmail.com>
2020-01-22 10:28:31 -0500

    Partially cleaned code, will carry on pushing to github and working on quizzingPage
    completion: done      version: 0.3.1
    branch_type: bugfix   version_type: patch
    branch: bugfix_v0.3.0--refactoring_code

--- src/app/App.js
+++ src/app/App.js
@@ -1,55 +1,38 @@
 // React Components
-import React, { Component } from 'react';
-import { Switch, Route } from "react-router-dom";
+import React, {Component} from 'react';
+import {Switch, Route} from "react-router-dom";
 
 // Assets
 import './App.scss';
 
 // App Components
-// import UserMenuPage from "./pages/userMenu/UserMenuPage.component";
 import Header from "./components/header/Header.component";
 import LoginPage from "./pages/login/LoginPage.component";
 import UserHomePage from "./pages/userHome/UserHomePage.component";
 import TestPage from "./pages/test/TestPage.component";
 import CreateQuizPage from "./pages/createQuiz/CreateQuizPage.component";
-// import store from "../redux/store";
 
 class App extends Component {
 
-  state = {
-    boolShowLoginPage: true,
-    arrQuizzes: []
-  };
+    state = {
+        boolShowLoginPage: true,
+    };
 
-  // componentDidMount() {
-  //   fetch('http://localhost:3000/api/v1/quizzes')
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         // debugger
-  //         this.setState({
-  //           quizzes: data
-  //         });
-  //       }).catch(console.error);
-  // }
+    render() {
+        return (
+            <div className="App">
+                {(this.state.boolShowLoginPage) && <Header/>}
 
-
-  render() {
-      // console.log(this.props.store);
-
-      return (
-        <div className="App">
-          {(this.state.boolShowLoginPage) && <Header/>}
-
-          <Switch>
-            {/*<Route path="/userMain" render={(routerProps) => <UserMenuPage quizzes={this.state.quizzes} {...routerProps}/>}/>*/}
-            <Route path="/login" component={LoginPage}/>
-            <Route path="/create-quiz" component={CreateQuizPage}/>
-            <Route path="/test" component={TestPage}/>
-            <Route path="/" component={UserHomePage}/>
-          </Switch>
-        </div>
-    );
-  }
+                <Switch>
+                    {/*<Route path="/userMain" render={(routerProps) => <UserMenuPage quizzes={this.state.quizzes} {...routerProps}/>}/>*/}
+                    <Route path="/login" component={LoginPage}/>
+                    <Route path="/create-quiz" component={CreateQuizPage}/>
+                    <Route path="/test" component={TestPage}/>
+                    <Route path="/" component={UserHomePage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
